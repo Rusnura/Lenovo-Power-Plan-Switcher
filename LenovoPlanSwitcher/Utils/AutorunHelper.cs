a7460d469772f5d590d95b46b07b47e24838f98d
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-07-23 02:42:39 +0700

    added 'AutoRunHelper' class from 'MediaPlayerPromptDisabler' project

---
+++ LenovoPlanSwitcher/Utils/AutorunHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenovoPlanSwitcher.Utils
+{
+    public class AutorunHelper
+    {
+        private const string APPLICATION_ID = "LenovoPowerPlanSwitcher";
+        private const string AUTORUN_REGISTRY_PATH = "Software\\Microsoft\\Windows\\CurrentVersion\\Run\\";
+
+        private static string applicationExecFile = "\"" + Assembly.GetEntryAssembly().Location + "\"";
+
+        public static bool SetApplicationAutorunState(bool state)
+        {
+            try
+            {
+                if (RegistryUtils.CheckKeyExists(APPLICATION_ID, AUTORUN_REGISTRY_PATH))
+                {
+                    RegistryUtils.RemoveKeyFromUserRegistry(APPLICATION_ID, AUTORUN_REGISTRY_PATH);
+                }
+
+                if (state)
+                {
+                    RegistryUtils.AddKeyToUserRegistry(APPLICATION_ID, applicationExecFile, AUTORUN_REGISTRY_PATH);
+                }
+                return true;
+            }
+            catch
+            {
+                // TODO: Log...
+                return false;
+            }
+        }
+
+        public static bool GetApplicationAutorunState()
+        {
+            return RegistryUtils.CheckKeyExists(APPLICATION_ID, AUTORUN_REGISTRY_PATH);
+        }
+    }
+}
