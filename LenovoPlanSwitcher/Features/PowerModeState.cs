73544ee3ec130e96326416f08bda94a9bfb6cecb
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-07-22 23:09:34 +0700

    Added 'PowerModeState'

---
+++ LenovoPlanSwitcher/Features/PowerModeState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenovoPlanSwitcher.Features
+{
+    public enum PowerModeState
+    {
+        Quiet,
+        Balance,
+        Performance
+    }
+
+    public class PowerModeFeature : AbstractWMIFeature<PowerModeState>
+    {
+        public PowerModeFeature() : base("SmartFanMode", 1)
+        {
+
+        }
+    }
+}
