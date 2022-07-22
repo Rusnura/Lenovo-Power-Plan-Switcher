585758b487d14de2e899b8b957215b8aebcac85c
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-07-23 02:23:27 +0700

    Added 'PowerPlan' class - just class with schema id and schema name

---
+++ LenovoPlanSwitcher/Features/Models/PowerPlan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenovoPlanSwitcher.Features.Models
+{
+    public class PowerPlan
+    {
+        public Guid powerPlanId { get; }
+        public string name { get; }
+
+        public PowerPlan(Guid powerPlanId, string name)
+        {
+            this.powerPlanId = powerPlanId;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
