e109f63780bf69fa67d72b5d8a98072ed50a8e0c
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-07-24 11:50:26 +0700

    Added 'PowerPlanSwitchService'

---
+++ LenovoPlanSwitcher/Services/PowerPlanSwitchService.cs
@@ -0,0 +1,55 @@
+﻿using LenovoPlanSwitcher.Features;
+using LenovoPlanSwitcher.Features.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenovoPlanSwitcher.Services
+{
+    public class PowerPlanSwitchService
+    {
+        private readonly PowerModeFeature _powerModeFeature = new PowerModeFeature();
+        private readonly PowerPlanFeature _powerPlanFeature = new PowerPlanFeature();
+
+        public List<PowerPlan> PowerPlans { get { return _powerPlanFeature.powerPlans; } }
+        private PowerModeState PreviousLenovoState { get; set; }
+        public PowerModeState CurrentLenovoState { get { return _powerModeFeature.GetState(); } private set { _powerModeFeature.SetState(value); } }
+        public PowerPlan CurrentWindowsPlan { get { return _powerPlanFeature.GetState(); } private set { _powerPlanFeature.SetState(value); } }
+        public PowerPlan QuietModePowerPlan { get; set; }
+        public PowerPlan BalanceModePowerPlan { get; set; }
+        public PowerPlan PerformanceModePowerPlan { get; set; }
+
+        public void UpdateWindowsPlanByLenovoPowerState(PowerModeState lenovoState)
+        {
+            if (PreviousLenovoState == lenovoState)
+                return;
+
+            PreviousLenovoState = lenovoState;
+
+            switch (PreviousLenovoState)
+            {
+                case PowerModeState.Quiet:
+                    if (QuietModePowerPlan.powerPlanId != Guid.Empty)
+                        CurrentWindowsPlan = QuietModePowerPlan;
+                break;
+
+                case PowerModeState.Balance:
+                    if (BalanceModePowerPlan.powerPlanId != Guid.Empty)
+                        CurrentWindowsPlan = BalanceModePowerPlan;
+                break;
+
+                case PowerModeState.Performance:
+                    if (PerformanceModePowerPlan.powerPlanId != Guid.Empty)
+                        CurrentWindowsPlan = PerformanceModePowerPlan;
+                break;
+
+                default:
+                    // NOP
+                break;
+            }
+
+        }
+    }
+}
